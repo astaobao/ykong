d2ee062ef10875228931685baf396343643b04c4
astaobao <3027219140@qq.com>
2020-04-08 20:41:43 +0800

    '更新README.md'

--- pages/tabbar/tabbar-5/tabbar-5.vue
+++ pages/tabbar/tabbar-5/tabbar-5.vue
@@ -1,15 +1,18 @@
 <template>
 	<view class="content">
-		学习的第一步
-		</br>
-		先敲一个Hello World吧!
-		</br>
-		</br>
-		</br>
-		<div>
+	
+		<div style="margin-top: 50px;margin-bottom: 50px;">
 			<img src="../../../static/img/github.png">
 			</br>
-			创作不易,欢迎前来<span style="color: red;">Star</span>
+			创作不易,欢迎前来 
+			<a href="https://github.com/astaobao/ykong" style="text-decoration:none;">
+			<mt-badge size="normal" style="color: red;">Star</mt-badge>
+				</a>
+		</div>
+		<div style="font-size: 15px;">
+		如果您有一个好的idea
+		</br>
+		欢迎留言
 		</div>
 	</view>
 </template>
